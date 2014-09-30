19a925d7cb84ac8ac0aedcf0c54ecf49ecf8944d
Jared Roesch <roeschinc@gmail.com>
2014-09-29 21:44:03 -0700

    Attempting to get full compile and link cycle working

--- src/raw/ruby.rs
+++ src/raw/ruby.rs
@@ -5,10 +5,15 @@
 
 use libc::*;
 
+/* These are hacks to get around Bindgen being broken */
+type va_list = ();
+type Struct_st_table = ();
+
 bindgen!(
-    "/Users/jroesch/.rbenv/versions/2.1.2/include/ruby-2.1.0/ruby.h",
+    "/Users/jroesch/.rbenv/versions/2.1.3/include/ruby-2.1.0/ruby.h",
     match="ruby.h",
-    link_static="ruby",
+    link_framework="ruby",
     emit_builtins=true,
-    clang_args="-I/Users/jroesch/.rbenv/versions/2.1.2/include/ruby-2.1.0/"
+    allow_unknown_types=true,
+    clang_args="-I/Users/jroesch/.rbenv/versions/2.1.3/include/ruby-2.1.0"
 )
