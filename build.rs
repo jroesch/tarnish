b1768e3987c6d259aa9cbcc2df7ba19229738e45
Jared Roesch <roeschinc@gmail.com>
2015-08-24 13:57:41 -0700

    WIP

--- build.rs
+++ build.rs
@@ -10,44 +10,24 @@ fn main() {
                    .create(true)
                    .open("src/raw/ruby.rs")
                    .unwrap();
-                   
+
     let mut builder = bindgen::builder();
     // builder.forbid_unknown_types();
     // builder.emit_builtins();
     builder.header("/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0/ruby.h");
-    builder.match_pat("ruby.h");
+    builder.match_pat("ruby");
+    builder.match_pat("select");
     builder.link_framework("ruby");
     builder.clang_arg("-I/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0");
     builder.clang_arg("-I/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0/x86_64-darwin14");
 
-    let mut file = OpenOptions::new()
-                   .write(true)
-                   .truncate(true)
-                   .create(true)
-                   .open("src/raw/ruby.rs")
-                   .unwrap();
+    file.write("use libc::{size_t, uint32_t};\n".as_bytes()).unwrap();
+    file.write("use libc::types::os::arch::posix88::pid_t;\n".as_bytes()).unwrap();
+    file.write("use std::os::unix::raw::{time_t, mode_t};\n".as_bytes()).unwrap();
+    file.write("use libc::types::os::common::posix01::timeval;\n".as_bytes()).unwrap();
+    file.write("pub type Struct_timeval = timeval;\n".as_bytes()).unwrap();
+    file.write("pub type va_list = ();".as_bytes()).unwrap();
 
-    file.write("use libc::{size_t, uint32_t};\n".as_bytes());
     let bindings = builder.generate().unwrap();
     bindings.write(Box::new(file)).unwrap();
-
-    // let mut builder = bindgen::builder();
-    // builder.forbid_unknown_types();
-    // builder.emit_builtins();
-    // builder.header("/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0/ruby/st.h");
-    // builder.match_pat("st.h");
-    // builder.link_framework("ruby");
-    // builder.clang_arg("-I/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0");
-    // builder.clang_arg("-I/Users/jroesch/.rbenv/versions/2.2.2/include/ruby-2.2.0/x86_64-darwin14");
-    //
-    // let mut file = OpenOptions::new()
-    //                .write(true)
-    //                .truncate(true)
-    //                .create(true)
-    //                .open("src/raw/ruby.rs")
-    //                .unwrap();
-    //
-    // file.write("use libc::{size_t, uint32_t};\n".as_bytes());
-    // let bindings = builder.generate().unwrap();
-    // bindings.write(Box::new(file)).unwrap();
 }
